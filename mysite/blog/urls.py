4b3232aea909a0744c894d4561f887d4bc43ffb8
Unknown <cupjoo@naver.com>
2018-07-16 21:22:52 +0900

    Blog 앱 생성

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -17,7 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('bookmark/', include('bookmark.urls')),
-    path('blog/', include('blog.urls')),
+
 ]
