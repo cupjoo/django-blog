20da91fbc4d9ec2027cffe212344a846ddeed8ca
Unknown <cupjoo@naver.com>
2018-07-17 00:33:31 +0900

    blog/post 모델 생성

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField('TITLE', max_length=50)
+    slug = models.SlugField('SLUG', max_length=50, unique=True, allow_unicode=True,
+                            help_text='one word for title alias.')
+    description = models.CharField('DESCRIPTION', max_length=100, blank=True,
+                                   help_text='simple description text.')
+    content = models.TextField('CONTENT')
+    create_date = models.DateTimeField('Create Date', auto_now_add=True)
+    modify_date = models.DateTimeField('Modify Date', auto_now=True)
+    
+    def __str__(self):
+        return self.title
