7bba654a903c080ff783f13f6f7f84de8b1e8d6c
Unknown <cupjoo@naver.com>
2018-07-17 01:03:57 +0900

    blog/PostLV 클래스 생성

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, \
+    MonthArchiveView, DayArchiveView, TodayArchiveView
+from blog.models import Post
 
-# Create your views here.
+
+class PostLV(ListView):
+    model = Post
+    template_name = 'blog/post_all.html'
+    context_object_name = 'posts'
+    paginate_by = 2
