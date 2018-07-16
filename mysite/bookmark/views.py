7fbcced72bed4f9eb87c2a1b3548b5ae9871ed82
Unknown <cupjoo@naver.com>
2018-07-16 21:10:32 +0900

    bookmark/BookmarkDV 클래스 생성

--- mysite/bookmark/views.py
+++ mysite/bookmark/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from bookmark.models import Bookmark
 
 
 class BookmarkLV(ListView):
+    model = Bookmark
+
+
+class BookmarkDV(DetailView):
     model = Bookmark
