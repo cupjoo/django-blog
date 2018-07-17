d88f82ae2bcd7269990a5ddd5c2bc7e1e049f7f0
Unknown <cupjoo@naver.com>
2018-07-18 01:02:04 +0900

    blog/URL conf 수정

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.PostLV.as_view(), name='index'),
     path('post/', views.PostLV.as_view(), name='post_list'),
     path('post/<slug:slug>/', views.PostDV.as_view(), name='post_detail'),
+
     path('archive/', views.PostAV.as_view(), name='post_archive'),
     path('<int:year>/', views.PostYAV.as_view(), name='post_year_archive'),
     path('<int:year>/<month>/', views.PostMAV.as_view(), name='post_month_archive'),
