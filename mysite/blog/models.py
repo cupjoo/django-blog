b6bf4be8b05e9000c25ac436f103ce88a44246b8
Unknown <cupjoo@naver.com>
2018-07-18 00:55:16 +0900

    blog/post 모델 수정
    url 및 이전, 다음 포스트 주소 반환 메소드 생성

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -13,3 +14,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=(self.slug,))
+
+    def get_previous_post(self):
+        return self.get_previous_by_modify_date()
+
+    def get_next_post(self):
+        return self.get_next_by_modify_date()
