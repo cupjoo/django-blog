70824f2889c34a96524eb2aea14713fb65fbd52e
Unknown <cupjoo@naver.com>
2018-07-18 16:56:03 +0900

    url 오류 수정
    app_name 등록

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from blog import views
 
+app_name = 'blog'
 urlpatterns = [
     path('', views.PostLV.as_view(), name='index'),
     path('post/', views.PostLV.as_view(), name='post_list'),
