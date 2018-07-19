2bc1e2e1a5e4615e71bae7778bee0674734db323
Unknown <cupjoo@naver.com>
2018-07-19 16:00:10 +0900

    admin 페이지 언어 변경

--- mysite/conf/settings.py
+++ mysite/conf/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Seoul'
 
