71e08c7cd203ec48fcc3bd81236a89e550c5f98b
Unknown <cupjoo@naver.com>
2018-07-19 14:54:45 +0900

    홈페이지 URL 수정

--- mysite/conf/urls.py
+++ mysite/conf/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from conf.views import HomeView
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
