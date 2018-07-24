30efd182de23487dbdaf2da43321dc68f1e170cc
Unknown <cupjoo@naver.com>
2018-07-24 23:49:21 +0900

    base 템플릿 수정
    로그인 기능 추가

--- mysite/conf/views.py
+++ mysite/conf/views.py
@@ -1,5 +1,18 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.base import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+
+class UserCreateView(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register_done')
+
+
+class UserCreateDoneTV(TemplateView):
+    template_name = 'registration/register_done.html'
