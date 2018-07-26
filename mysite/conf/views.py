c2f5060b711d031c20828502f0c12bd32f2d0abe
Unknown <cupjoo@naver.com>
2018-07-26 11:10:10 +0900

    LoginRequiredMixin 클래스 추가

--- mysite/conf/views.py
+++ mysite/conf/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
@@ -16,3 +17,10 @@ class UserCreateView(CreateView):
 
 class UserCreateDoneTV(TemplateView):
     template_name = 'registration/register_done.html'
+
+
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        return login_required(view)
