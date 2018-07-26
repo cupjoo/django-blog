a733590facca4385d8687c5e4f36669dc6443c0c
Unknown <cupjoo@naver.com>
2018-07-26 11:10:35 +0900

    포스트 수정 기능 뷰 추가

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,13 @@
 from django.db.models import Q
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, FormView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, FormView, UpdateView, CreateView, DeleteView
 from django.views.generic.dates import ArchiveIndexView, YearArchiveView, \
     MonthArchiveView, DayArchiveView, TodayArchiveView
 
 from blog.forms import PostSearchForm
 from blog.models import Post
+from conf.views import LoginRequiredMixin
 
 
 class PostLV(ListView):
@@ -63,3 +65,32 @@ class SearchFormView(FormView):
         context['object_list'] = post_list
 
         return render(self.request, self.template_name, context)
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'slug', 'description', 'content', 'tag']
+    initial = {'slug': 'auto-filling-do-not-input'}
+    success_url = reverse_lazy('blog:index')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(PostCreateView, self).form_valid(form)
+
+
+class PostChangeLV(LoginRequiredMixin, ListView):
+    template_name = 'blog/post_change_list.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(owner=self.request.user)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView) :
+    model = Post
+    fields = ['title', 'slug', 'description', 'content', 'tag']
+    success_url = reverse_lazy('blog:index')
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView) :
+    model = Post
+    success_url = reverse_lazy('blog:index')
