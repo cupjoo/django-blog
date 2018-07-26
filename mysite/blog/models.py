14e456190f3abffb31c35afd9d73e51e2f2bba2f
Unknown <cupjoo@naver.com>
2018-07-26 21:29:35 +0900

    blog 객체 URL 반환 형식 변경

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -23,11 +24,14 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=(self.slug,))
+
     def get_previous_post(self):
-        return self.get_previous_by_modify_date()
+        return self.get_previous_by_modify_date().get_absolute_url()
 
     def get_next_post(self):
-        return self.get_next_by_modify_date()
+        return self.get_next_by_modify_date().get_absolute_url()
 
     def save(self, *args, **kwargs):
         if not self.id:
